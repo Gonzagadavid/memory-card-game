65e0a70762a56c9ba5d2ff57ddecd90074202ad1
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-22 23:15:03 -0300

    iniciado a estrutura da página

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 import createDivs from './functions/createDivs.js';
 
 const contentContainer = document.getElementById('content-container');
+
+const statesContainer = createDivs('states-container');
+contentContainer.appendChild(statesContainer);
+
+const gameContainer = createDivs('game-container');
+contentContainer.appendChild(gameContainer);
