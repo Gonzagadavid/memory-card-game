1854b5e088226b0bd4bb1181a404c2bbad04ae5e
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-22 23:09:39 -0300

    criado pastas e arquivos de acordo com suas reposabilidade, escita a função responsavel por criar as divs

---
+++ js/functions/createDivs.js
@@ -0,0 +1,5 @@
+export default function createDivs(classText) {
+  const div = document.createElement('div');
+  div.className = classText;
+  return div;
+}
---
+++ js/index.js
@@ -0,0 +1,3 @@
+import createDivs from './functions/createDivs.js';
+
+const contentContainer = document.getElementById('content-container');
